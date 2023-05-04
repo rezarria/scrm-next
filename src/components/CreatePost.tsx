7fce390788c0cb3877a1831729a37ae201b799f0
rezarria <votanamcb@outlook.com>
2023-05-04 12:25:19 +0700

    up

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -7,15 +7,35 @@ import Image from 'next/image'
 
 export default function CreatePost () {
 	const nutH = useRef<HTMLDivElement>(null)
+	const modal = useRef<HTMLDivElement>(null)
+	const nutHuy = useRef<HTMLButtonElement>(null)
+	const overlay = useRef<HTMLDivElement>(null)
 	useEffect(() => {
-		console.log(nutH.current)
+		if (nutHuy.current === null) throw ''
+		nutHuy.current.onclick = ev => {
+			if (modal.current === null) throw ''
+			modal.current.classList.add('hidden')
+		}
+		if (nutH.current === null) throw ''
+		nutH.current.onclick = ev => {
+			if (modal.current === null) throw ''
+			modal.current.classList.remove('hidden')
+		}
+		if (overlay.current === null) throw ''
+		overlay.current.onclick = ev => {
+			if (modal.current === null) throw ''
+			modal.current.classList.add('hidden')
+		}
 	}, [])
 	return <div className='bg-white mb-1'>
-		<div className='px-16 py-10 w-full'>
-			<div className='rounded w-full h-12 border cursor-pointer' ref={nutH}></div>
+		<div className='px-16 py-5 w-full'>
+			<div ref={nutH} className='rounded w-full h-12 border cursor-pointer'>
+				<span></span>
+			</div>
 		</div>
-		<div className='z-50 fixed w-full h-full top-0 left-0 bg-neutral-700/80'>
-			<div className='w-1/2 mx-auto top-1/2 relative -translate-y-1/2 bg-white p-5 rounded'>
+		<div ref={modal} className='hidden z-50 fixed w-full h-full top-0 left-0'>
+			<div ref={overlay} className='z-10 bg-neutral-700/80 absolute w-full h-full cursor-pointer'></div>
+			<div className='z-20 w-[800px] mx-auto top-12 relative bg-white p-5 rounded'>
 				<h1></h1>
 				<textarea className='rounded w-full min-h-[200px] hover:outline-blue-600 outline-0 p-2 resize-none'/>
 				<div className='flex flex-row gap-1 border-t-2 pt-2'>
@@ -36,7 +56,7 @@ export default function CreatePost () {
 					</div>
 					<div className='flex flex-row gap-2 justify-end'>
 						<button className='flex-1 rounded bg-blue-300 p-1 text-white font-extrabold hover:duration-300 duration-150 hover:bg-blue-500'>Gửi</button>
-						<button className='rounded bg-red-300 p-1 min-w-[100px] text-white font-extrabold hover:duration-300 duration-150 hover:bg-red-500'>Hủy</button>
+						<button ref={nutHuy} className='rounded bg-red-300 p-1 min-w-[100px] text-white font-extrabold hover:duration-300 duration-150 hover:bg-red-500'>Hủy</button>
 					</div>
 				</div>
 			</div>
